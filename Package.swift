02e444e585ad96854af29e41fe579f4a19775aa0
pcbeard <pcbeard@mac.com>
2021-09-23 15:33:39 -0700

    Build as a shared library

--- Package.swift
+++ Package.swift
@@ -37,9 +37,7 @@ let package = Package(
     name: "SDL_gfx",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "SDL_gfx",
-            targets: ["SDL_gfx"]),
+        .library(name: "SDL_gfx", type: .dynamic, targets: ["SDL_gfx"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
