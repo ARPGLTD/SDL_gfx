c7458c8e28a6739f440fa3dad69565c9de43836d
pcbeard <pcbeard@mac.com>
2021-09-25 02:52:57 -0700

    Add os(Windows) build
    Building static on Windows for now
    
    With clang, no need to redefine lrint().

--- Package.swift
+++ Package.swift
@@ -6,30 +6,50 @@ import PackageDescription
 struct TargetConfiguration {
     var cflags : [CSetting] = []
     var lflags : [LinkerSetting] = []
+    var libraryType : Product.Library.LibraryType?
     var sourcePaths : [String] = []
     var excludePaths : [String] = []
 }
 
-#if os(macOS)
-
 var gfxConfig = TargetConfiguration(
-    cflags: [
-//        .define("SDL_IMAGE_USE_COMMON_BACKEND", to: "1"),
-//        .define("LOAD_BMP", to: "1"),
-    ],
     sourcePaths: [
         "src/SDL2_framerate.c",
         "src/SDL2_gfxPrimitives.c",
         "src/SDL2_imageFilter.c",
         "src/SDL2_rotozoom.c"
-    ],
-    excludePaths: [
     ]
 )
 
-#else
+#if os(macOS)
+
+gfxConfig.cflags = [
+    // .define("SDL_IMAGE_USE_COMMON_BACKEND", to: "1"),
+    // .define("LOAD_BMP", to: "1"),
+]
+
+gfxConfig.libraryType = .dynamic
+
+#elseif os(Linux)
 
 var gfxConfig = TargetConfiguration()
+
+gfxConfig.cflags = [
+    // .define("SDL_IMAGE_USE_COMMON_BACKEND", to: "1"),
+    // .define("LOAD_BMP", to: "1"),
+]
+
+#elseif os(Windows)
+
+gfxConfig.cflags = [
+    // .define("SDL_IMAGE_USE_COMMON_BACKEND", to: "1"),
+    // .define("LOAD_BMP", to: "1"),
+    .unsafeFlags(["-msse3"]),
+]
+
+gfxConfig.lflags = [
+    .linkedLibrary("swiftCore"),
+    .linkedLibrary("USER32"),
+]
 
 #endif
 
@@ -37,7 +57,7 @@ let package = Package(
     name: "SDL_gfx",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "SDL_gfx", type: .dynamic, targets: ["SDL_gfx"]),
+        .library(name: "SDL_gfx", type: gfxConfig.libraryType, targets: ["SDL_gfx"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
