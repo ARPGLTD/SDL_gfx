adfd2d21551d3f9d3ef87d7e31da5642c63f2a60
pcbeard <pcbeard@mac.com>
2021-10-20 18:56:31 -0700

    Update to 5.5, stop using deprecated package(name:url:_ Requirement)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -63,7 +63,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "SDL", url: "git@github.com:ARPGLTD/SDL.git", .branch("SwiftPackage")),
+        .package(url: "git@github.com:ARPGLTD/SDL.git", branch: "SwiftPackage"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
